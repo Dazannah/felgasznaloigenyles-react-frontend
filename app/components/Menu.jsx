068d3a69f667c7f8e394c88fff38049fdb052031
Dazannah <palmal648@gmail.com>
2023-06-25 13:40:09 +0200

    menu style

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -43,57 +43,52 @@ function Menu() {
 
   return (
     <>
-      <div key="menuWrapper" id="menu-wrapper">
-        <nav key="menu" id="main-menu">
-          <ul className="group">
-            <li>
-              {" "}
-              <Link id="createNewBtn" onClick={e => setAcctiveBtn(e.target.id)} key="createNewBtn" className="menu-btn" to="/">
-                Új felvétel
-              </Link>
-            </li>
-            <li>
-              {" "}
-              <Link id="listUsersBtn" onClick={e => setAcctiveBtn(e.target.id)} key="listUsersBtn" className="menu-btn" to="/list-users">
-                Felhasználók listázása
-              </Link>
-            </li>
-            <li>
-              {" "}
-              <Link id="listRequestsBtn" onClick={e => setAcctiveBtn(e.target.id)} key="listRequestsBtn" className="menu-btn" to="/list-requests">
-                Kérelmek listázása
-              </Link>
-            </li>
-            <li>
-              {" "}
-              <Link id="allowedRequestsBtn" onClick={e => setAcctiveBtn(e.target.id)} key="allowedRequestsBtn" className="menu-btn" to="/list-allowed-requests">
-                Engedélyezett kérelmek listázása
-              </Link>
-            </li>
-            <li>
-              {" "}
-              <Link id="completedRequestsBtn" onClick={e => setAcctiveBtn(e.target.id)} key="completedRequestsBtn" className="menu-btn" to="/list-completed-requests">
-                Lezárt kérelmek
-              </Link>
-            </li>
-            <li>
-              {" "}
-              <Link id="distributinListBtn" onClick={e => setAcctiveBtn(e.target.id)} key="distributinListBtn" className="menu-btn" to="/asdasd">
-                Terjesztési lista ígénylés
-              </Link>
-            </li>
-            <li>
-              {" "}
-              <Link id="logOutBtn" key="logOutBtn" className="menu-btn" onClick={e => logout(e)} to="/">
-                Kijelentkezés
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div key="title" id="title">
-        <p>{`${appState.siteLocation}`}</p>
-      </div>
+      <nav key="menu" id="main-menu">
+        <ul className="group">
+          <li>
+            {" "}
+            <Link id="createNewBtn" onClick={e => setAcctiveBtn(e.target.id)} key="createNewBtn" to="/">
+              Új felvétel
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link id="listUsersBtn" onClick={e => setAcctiveBtn(e.target.id)} key="listUsersBtn" to="/list-users">
+              Felhasználók listázása
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link id="listRequestsBtn" onClick={e => setAcctiveBtn(e.target.id)} key="listRequestsBtn" to="/list-requests">
+              Kérelmek listázása
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link id="allowedRequestsBtn" onClick={e => setAcctiveBtn(e.target.id)} key="allowedRequestsBtn" to="/list-allowed-requests">
+              Engedélyezett kérelmek listázása
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link id="completedRequestsBtn" onClick={e => setAcctiveBtn(e.target.id)} key="completedRequestsBtn" to="/list-completed-requests">
+              Lezárt kérelmek
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link id="distributinListBtn" onClick={e => setAcctiveBtn(e.target.id)} key="distributinListBtn" to="/asdasd">
+              Terjesztési lista ígénylés
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link id="logOutBtn" key="logOutBtn" onClick={e => logout(e)} to="/">
+              Kijelentkezés
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }

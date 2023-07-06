99c18787b63ca5bf0d6c3b5a681cc6c655f804a6
Dazannah <palmal648@gmail.com>
2023-07-06 18:08:24 +0200

    createTextArea if made nicer

--- app/components/CreateNewTextarea.jsx
+++ app/components/CreateNewTextarea.jsx
@@ -4,20 +4,7 @@ import FormDispatchContext from "../FormDispatchContext.jsx"
 function CreateNewTextarea(props) {
   const formDispatch = useContext(FormDispatchContext)
 
-  let showTextArea
-  if (props.request) {
-    if (props.request.createTextArea) {
-      if (props.request.createTextArea.trim(" ") != "") {
-        showTextArea = true
-      } else {
-        showTextArea = false
-      }
-    } else {
-      showTextArea = false
-    }
-  } else {
-    showTextArea = true
-  }
+  const showTextArea = props.request ? props.request.createTextArea && props.request.createTextArea.trim() != "" : true
 
   if (showTextArea) {
     return (

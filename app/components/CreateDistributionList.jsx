326d187b9073a7a3aab100eaaf06d3e986a7ba4f
dazannah <palmal648@gmail.com>
2023-07-07 15:08:14 +0200

    distribution list create

--- app/components/CreateDistributionList.jsx
+++ app/components/CreateDistributionList.jsx
@@ -1,38 +1,123 @@
-import React, { useState } from "react"
+import React, { useEffect, useState, useContext } from "react"
+import Axios from "axios"
+
+import StateContext from "../StateContext.jsx"
+import DispatchContext from "../DispatchContext.jsx"
 
 function CreateDistributionList(props) {
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
+
   const [inputFieldNumber, setInputFieldNumber] = useState(1)
+  const [generateInputFieldsNow, setGenerateInputFieldsNow] = useState(true)
+  const [inputFields, setInputField] = useState([])
   const [fieldToAdd, setFieldToAdd] = useState(1)
 
-  function generateInputFields() {
-    const inputFields = []
-    for (let i = 0; i < inputFieldNumber; i++) {
-      inputFields.push(
-        <>
-          <input id={i} key={`${i}input`} type="text" />
-          <br />
-        </>
-      )
+  useEffect(()=>{
+    function generateInputFields() {
+      const inputFieldsTemp = []
+      for (let i = 0; i < inputFieldNumber; i++) {
+        inputFieldsTemp.push(
+          <>
+            <input id={i} name={`email${i}`} key={`${i}input`} type="text" />
+            <br />
+          </>
+        )
+      }
+      setInputField(inputFieldsTemp)
     }
-    return inputFields
-  }
+
+    generateInputFields()
+    setGenerateInputFieldsNow(false)
+  },[generateInputFieldsNow])
+
 
   function addField() {
-    console.log(inputFieldNumber + parseInt(fieldToAdd))
-    setInputFieldNumber(inputFieldNumber + fieldToAdd)
+    if(parseInt(fieldToAdd)>0 && parseInt(fieldToAdd) < 150){
+      setInputFieldNumber(inputFieldNumber + parseInt(fieldToAdd))
+      setGenerateInputFieldsNow(true)
+    }
+
   }
 
   function deleteField() {
-    if (inputFieldNumber - fieldToAdd <= 1 && inputFieldNumber - fieldToAdd > 0) {
-      setInputFieldNumber(inputFieldNumber - 1)
-    } else if (inputFieldNumber - fieldToAdd > 1) {
-      setInputFieldNumber(inputFieldNumber - fieldToAdd)
+    if(parseInt(fieldToAdd)>0 && parseInt(fieldToAdd) < 150){
+      if(inputFieldNumber - parseInt(fieldToAdd) > 1){
+        setInputFieldNumber(inputFieldNumber - parseInt(fieldToAdd))
+        setGenerateInputFieldsNow(true)
+      }else if(inputFieldNumber - parseInt(fieldToAdd) <= 1){
+        setInputFieldNumber(1)
+        setGenerateInputFieldsNow(true)
+      }
+    }
+
+
+  }
+
+  function getFormData(e){
+    const formData = new FormData(e.target)
+    const values = Object.fromEntries(formData.entries())
+
+    return values
+  }
+
+  async function validateData(dataToValidate){
+    const error = []
+    if(dataToValidate.distributionListAddy == ""){
+      appDispatch({ type: "flashMessageWarning", value: "Terjesztési lista cím megadása kötelező." })
+      return
+    }
+
+    for(let i = 0; i<inputFieldNumber; i++){
+      if(dataToValidate[`email${i}`] == "") error.push(`${i+1}. email címet meg kell adni.`)
+    }
+
+    if(error.length > 0){
+      appDispatch({ type: "flashMessageWarning", value: error })
+      return
+    }
+
+    return dataToValidate
+
+  }
+
+  async function sendData(dataToSend){
+    try{
+      const response = await Axios.post(
+        "/create-new-distribution-list",
+        {
+          dataToSend,
+          eMailFieldNumber: inputFieldNumber
+        },
+        {
+          headers: {
+            authorization: `Bearer ${appState.user.token}`
+          }
+        }
+      )
+
+      return response
+
+    }catch(err){
+      appDispatch({ type: "flashMessageWarning", value: JSON.stringify(err) })
     }
   }
 
   async function submitHandler(e) {
     e.preventDefault()
-    console.log(e)
+    const dataToSendTmp = getFormData(e)
+    const dataToSend = await validateData(dataToSendTmp)
+
+    if(dataToSend){
+      const response = await sendData(dataToSend)
+
+      if(response.data.errors){
+        appDispatch({ type: "flashMessageWarning", value: response.data.errors })
+      }else{
+        appDispatch({ type: "flashMessageSuccess", value: "Terjesztési lista igénylés létrehozása sikeres." })
+      }
+    }
+
   }
 
   return (
@@ -49,15 +134,17 @@ function CreateDistributionList(props) {
       <form onSubmit={e => submitHandler(e)}>
         <label for="distribution-list">Terjesztési lista címe:</label>
         <br />
-        <input id="distribution-list-addy" type="text" />
+        <input id="distribution-list-addy" name="distributionListAddy" type="text" />
         <br />
         <br />
         <label for="distribution-list-emails">E-mail cím:</label>
         <br />
-        {generateInputFields()}
+        {inputFields.map(field =>{
+          return field
+        })}
 
         <br />
-        <button className="form-submit-input round-corner" id="createDistributionListButton" type="submit" value="Küldés">
+        <button className="form-submit-input round-corner" type="submit" value="Küldés">
           Küldés
         </button>
       </form>

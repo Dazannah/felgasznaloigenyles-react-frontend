5a63ff98b0504299ea080b0070d52c1df047a7b6
dazannah <palmal648@gmail.com>
2023-07-14 13:49:27 +0200

    a plus turnery deleted

--- app/components/ColumnBlueprint.jsx
+++ app/components/ColumnBlueprint.jsx
@@ -6,7 +6,7 @@ function ColumnBlueprint(props) {
       {props.arrays.map((element, index) => {
         return (
           <div key={index}>
-            <input defaultChecked={props.request ? props.request[props.arrayPosition][index].value : ""} {...(props.listOut ? { checked: props.request[`${props.arrayPosition}`][index].value, disabled: true } : "")} onChange={e => props.states[index][1]({ ...props.states[index][0], value: e.target.checked })} type="checkbox" name={element.name} id={element.id} />
+            <input  {...(props.listOut ? { defaultChecked: props.request[`${props.arrayPosition}`][index].value, disabled: true } : "")} onChange={e => props.states[index][1]({ ...props.states[index][0], value: e.target.checked })} type="checkbox" name={element.name} id={element.id} />
             <label htmlFor={element.id}>{element.value}</label>
             <br />
           </div>
